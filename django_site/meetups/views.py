8e98b520ef3084a5a24f1050e8913e3ba124cc7a
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-19 13:44:33 +0200

    start 30:00
    30: 00 min inde i videon
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

---
+++ django_site/meetups/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse('look at my site!!')
+
+
+
+
+
+
+
+
+    # fra https://www.youtube.com/watch?v=t7DrJqcUviA&list=RDCMUCSJbGtTlrDami-tDGPUV9-w&index=3
