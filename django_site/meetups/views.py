ba87202e11cc1ba661f2502453e4670fc8faf945
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-19 16:48:47 +0200

    1:03
    1: 03 timer inde i videoen
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/meetups/views.py
+++ django_site/meetups/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('look at my site!!')
+    meetups =[
+        { 'title': 'A first meetup', 
+        'location': 'rundeperler', 
+        'slug': 'første ide'},
+        { 'title': 'A second meetup', 
+        'location': 'rørperler', 
+        'slug': 'nye måder ide' },
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
 
 
 
