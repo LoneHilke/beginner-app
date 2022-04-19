8450332980d6aef6e926200406c1d6c5eb008f26
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-19 19:53:16 +0200

    problemer her
    Dynamic Paths 01:15:17
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django_site/meetups/views.py
+++ django_site/meetups/views.py
@@ -6,15 +6,28 @@ def index(request):
     meetups =[
         { 'title': 'A first meetup', 
         'location': 'rundeperler', 
-        'slug': 'første ide'},
+        'slug': 'a-first-meetup'},
         { 'title': 'A second meetup', 
         'location': 'rørperler', 
-        'slug': 'nye måder ide' },
+        'slug': 'a-second-meetup' },
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
         'meetups': meetups
     })
+
+
+def meetup_details(request, meetup_slug):
+    print(meetup_slug)
+    selected_meetup = {
+        'title': 'A first Meetup', 
+        'description': 'This is the first test',
+        }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
+
 
 
 
